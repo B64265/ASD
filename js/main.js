e9fb6dbde6463dcf6c405e16518db7d6513e4e1c
Jeremy Goldman <jeremygoldman@fullsail.edu>
2013-07-21 22:34:01 -0500

    XML and JSON

--- js/main.js
+++ js/main.js
@@ -5,8 +5,67 @@
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
 	
+	var getData = function(){
+
+};
+
+//Find value of radio button
+	function getSelectedRadio(){
+		
+		var radioButtons = document.forms[0].priority;
+		for(var i=0; i<radioButtons.length; i++){
+			
+			if(radioButtons[i].checked){
+				priorityValue = radioButtons[i].value;
+			}
+		}
+	}
+
+
+
+function deleteItem(){
+		
+		var verify = confirm("Are you sure you want to delete this reminder?");
+		
+		if(verify){
+			
+			localStorage.removeItem(this.key);
+			alert("Reminder was deleted!");
+			window.location.reload();
+			
+		}else{
+			
+			alert("Reminder was NOT deleted.");
+		}
+		
+	}
 	
-		var myForm = $('#reminderForm'),
+function editItem(key){
+		
+		// Get data from Local Storage
+		var value = localStorage.getItem(key);
+		var reminder = JSON.parse(value);
+		
+		
+		$('#title').val(reminder.remindTitle);
+		$('#groups').val(reminder.group);
+		$('#due').val(reminder.dueDate);
+		$('#recurrence').val(reminder.recurrence);
+		$('#description').val(reminder.description);
+		
+		if(reminder.priority == "Yes"){
+			
+			$('priority').setAttribute("checked", "checked");
+		}
+		
+		//Change Submit button value to edit button
+		$('#submitButton').text = "Edit Reminder";
+		var editSubmit = $('#submitButton');
+		
+		//Save the key created
+		editSubmit.on('click', function(data){
+			
+			var myForm = $('#reminderForm'),
 			reminderErrorLink = $('#reminderErrorLink');
 		 
 		    myForm.validate({
@@ -25,34 +84,19 @@ $('#home').on('pageinit', function(){
 			submitHandler: function() {
 			var data = myForm.serializeArray();
 			storeData(data);
-		}
-	});
-	
-	
-});
-
-//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-
-var getData = function(){
-
-};
-
-//Find value of radio button
-	function getSelectedRadio(){
-		
-		var radioButtons = document.forms[0].priority;
-		for(var i=0; i<radioButtons.length; i++){
-			
-			if(radioButtons[i].checked){
-				priorityValue = radioButtons[i].value;
 			}
-		}
-	}
 
-
-var storeData = function(data){
+			
+			
+		})})
+		editSubmit.key = reminder.id;
 		
-		var id = Math.floor(Math.random()*1000000);
+	};
+
+	
+var storeData = function(){
+	
+	var id = Math.floor(Math.random()*1000000);
 	
 		getSelectedRadio();
 		
@@ -60,11 +104,12 @@ var storeData = function(data){
 		//Object properties has array with label and value.
 		
 		var item 				= {};
-		item.group 				= ["Group:", $('#category').val()];
-		item.remindTitle 		= ["Reminder Title:", $('#title').val()];
-		item.dueDate			= ["Due Date:", $('#due').val()];
-		item.priority			= ["Priority:", priorityValue];
-		item.description		= ["Description:", $('#description').val()];
+		item.group 				= $('#category').val();
+		item.remindTitle 		= $('#title').val();
+		item.dueDate			= $('#due').val();
+		item.priority			= priorityValue;
+		item.description		= $('#description').val();
+		item.id					= id;
 		
 		
 		//Save to Local Storage
@@ -74,26 +119,222 @@ var storeData = function(data){
 		console.log(id, JSON.stringify(item));
 
 	
-}; 
 
-var	deleteItem = function (key){
-	localStorage.removeItem(key);
+	
 };
+
+$('#submitButton').on('click', function(data){
+
+			var myForm = $('#reminderForm'),
+			reminderErrorLink = $('#reminderErrorLink');
+		 
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+				reminderErrorLink.click();
+				var html = '';
+				for(var key in validator.submitted){
+					var label = $('label[for^="'+ key +'"]').not('[generated]');
+					var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+					var fieldName = legend.length ? legend.text() : label.text();
+					html += '<li>' + fieldName +'</li>';
+				};
+				
+				$("#reminderErrors ul").html(html);
+			},
+			submitHandler: function() {
+			var data = myForm.serializeArray();
+			storeData(data);
+			}
+	
+})});
+
+$('#clearData').on('click', function(){
+
+	var verify = confirm("Are you sure you want to delete all reminders?");
+		
+		if(verify){
+			
+			localStorage.clear();
+			alert("localStorage has been cleared!!");
+			
+		}else{
+			
+			alert("localStorage was NOT cleared.");
+		}
+		
+});
+
+$('#reset').on('click', function(){	
+		
+		$('#category').val();
+		$('#title').val();
+		$('#due').val();
+		priorityValue = 0;
+		$('#description').val();
+		
+		getCurrentDate();
+		
+});
+
+$('#showData').on('click', function(){
+
+
+	if ((this.id === "Personal") || (this.id === "Work") || (this.id === "Other")) {
+			var appendLocation = $('#browseReminderList');
+			$('#browseReminderList').innerHTML = "";
+			console.log("Running like it should....");
+			filterCats = this.id;
+			browseCheck = true;
+		} else {
+			var appendLocation = $('#reminderList');
+			$('#reminderList').innerHTML = "";
+			console.log("Running like it should.... From SEARCH");
+			browseCheck = false;
+		}
+	for (var i = 0, j = localStorage.length; i < j; i++) {
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var obj = JSON.parse(value);
+			
+			if (browseCheck) {
+			console.log(obj.group[1]);
+				if (obj.group[1] === filterCats) {
+					readyCheck = true;
+				} else {
+					readyCheck = false;
+				}
+			} else {
+				readyCheck = true;
+			}
+			
+			if (readyCheck) {
+				if(browseCheck){
+				
+				var newImg = document.createElement('img');
+				var setSrc = newImg.setAttribute("src", "img/"+ filterCats +".png");
+				newImg.setAttribute("height", "128");
+				newImg.setAttribute("width", "128");
+				
+
+				var makeEntry = document.createElement('ul');
+				makeEntry.setAttribute("data-role", "listview");
+				appendLocation.appendChild(makeEntry);
+				var makeNewLi = document.createElement('li');
+				makeEntry.appendChild(makeNewLi);
+				makeNewLi.appendChild(newImg);
+				var makeH3 = document.createElement('h3');
+				makeH3.innerHTML = obj.remindTitle[1];
+				makeNewLi.appendChild(makeH3);
+				var makeP = document.createElement('p');
+				makeP.innerHTML = obj.description[1];
+				makeNewLi.appendChild(makeP);
+				makeEntry.setAttribute("id", key);	
 					
-var clearLocal = function(){
+				
+							
+				/*
+// Create List of Reminder Details
+				var makeList = document.createElement('ul');
+				makeEntry.appendChild(makeList);
+				for (var k in obj) {
+					var makeLi = document.createElement('li');
+					makeList.appendChild(makeLi);
+					var optSubText = obj[k][0]+ " " + obj[k][1];
+					makeLi.innerHTML = optSubText;
+					
+				}
+*/
+			/*
+	//Buttons set up for remove and edit
+				var buttonHolder = document.createElement('div');
+				buttonHolder.setAttribute("class", "ui-grid-a");
+				var editButtonDiv = document.createElement('div');
+				editButtonDiv.setAttribute("class", "ui-block-a");
+				var editButton = document.createElement('a');
+				editButton.setAttribute("data-role", "button");
+				editButton.setAttribute("href", "#reminder");
+				editButton.innerHTML = "Edit";
+				editButton.key = key;
+				var removeButtonDiv = document.createElement('div');
+				removeButtonDiv.setAttribute("class", "ui-block-b");
+				var removeButton = document.createElement('a');
+				removeButton.setAttribute("data-role", "button");
+				removeButton.setAttribute("href", "#");
+				removeButton.innerHTML = "Delete";
+				removeButton.key = key;
+				makeEntry.appendChild(buttonHolder);
+				buttonHolder.appendChild(editButtonDiv);
+				buttonHolder.appendChild(removeButtonDiv);
+				editButtonDiv.appendChild(editButton);
+				removeButtonDiv.appendChild(removeButton);
+				editButton.addEventListener("click", editItem);
+				removeButton.addEventListener("click", deleteItem);
+*/
+					
+				}else{
+				
+			/*
+	var newImg = document.createElement('img');
+				var setSrc = newImg.setAttribute("src", "img/"+ filterCats +".png");
+				newImg.setAttribute("height", "128");
+				newImg.setAttribute("width", "128");
+*/
+				
 
-};
+				var makeEntry = document.createElement('ul');
+				makeEntry.setAttribute("data-role", "listview");
+				$('#reminderList').append(makeEntry);
+				//appendLocation.appendChild(makeEntry);
+				var makeNewLi = document.createElement('li');
+				makeEntry.appendChild(makeNewLi);
+				//makeNewLi.appendChild(newImg);
+				var editButton = document.createElement('a');
+				editButton.setAttribute("href", "#home");
+				editButton.id = obj.id;
+				var makeH3 = document.createElement('h6');
+				makeH3.innerHTML = obj.remindTitle;
+				editButton.appendChild(makeH3);
+				var makeP = document.createElement('p');
+				makeP.innerHTML = obj.description;
+				editButton.appendChild(makeP);
+				makeNewLi.appendChild(editButton);
+				makeEntry.setAttribute("id", key);
+				console.log(obj.id);
+/*
+
+
+				//Buttons set up for remove and edit
+				var editButton = document.createElement('a');
+				editButton.setAttribute("data-role", "button");
+				editButton.setAttribute("href", "#reminder");
+				editButton.setAttribute("data-icon", "gear");
+				editButton.key = key;
+				
+				
+				makeEntry.appendChild(editButton);
+*/
+				
+				editButton.addEventListener("click", editItem(obj.id));
+			}
+			}}		
+});
+
+
 
 var getCurrentDate = function(){
 	var tD = new Date();
 	var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 	var datestr = monthNames[tD.getMonth()] + " " + tD.getDate()  + ", " + tD.getFullYear();
-	
-	var due = document.getElementById("due");
-		due.setAttribute("value", datestr);
+	$('#due').val(datestr);
+		
 	
 	//console.log("Success!");
 	
 };
+
+});
+
+
+
 
 
